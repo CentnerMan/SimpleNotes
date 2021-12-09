437b6d8f54cb0da660c16a84f548fc05532a1ac7
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2021-12-09 19:16:07 +0300

    Добавлен проект фрагмента для настроек.

---
+++ app/src/main/java/ru/vlsv/simplenotes/ui/preferences/PreferencesFragment.java
@@ -0,0 +1,30 @@
+package ru.vlsv.simplenotes.ui.preferences;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import ru.vlsv.simplenotes.R;
+
+public class PreferencesFragment extends Fragment {
+
+    public PreferencesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_preferences, container, false);
+    }
+
+}
